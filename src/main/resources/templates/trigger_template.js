62b764afa202b7a76567a2fd9f32298bd912859f
yourdeath <eugeneboyarnikov@gmail.com>
2022-12-20 15:04:54 +0300

    Поправлен шаблон скрипта

--- src/main/resources/templates/trigger_template.js
+++ src/main/resources/templates/trigger_template.js
@@ -1,15 +1,15 @@
 #foreach($trigger in $triggers)
 let elements = querySelectorAll("$trigger.getQuerySelector()");
 for (let i = 0; i < elements.length; i++) {
-  data = {
+  const data = {
     unitId: $trigger.getUnitId(),
     containerId: $trigger.getContainerId(),
     ASID: $trigger.getASID(),
     triggerId: $trigger.getTriggerId(),
     event: $trigger.getEvent(),
   }
-  elements[i].addEventListener($trigger.getEvent(), () => {
-    const response = await fetch(url, {
+  elements[i].addEventListener($trigger.getEvent(), async () => {
+    const response = await fetch(localhost:8080/api/trigger, {
       method: 'POST',
       mode: 'cors',
       cache: 'no-cache',
